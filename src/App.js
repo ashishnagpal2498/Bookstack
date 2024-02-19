55b86d5987da85caa438256be700ee8e7db97c73
unknown <ashish.nagpal@dal.ca>
2024-02-19 16:20:47 -0400

    Navbar :sparkle:

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Navigation } from './components/layouts/Navigation';
+
+const App = () => {
+
+  return (
+   <div>
+    <Navigation />
+   </div>
+  );
+}
+
+export default App;
