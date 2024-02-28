bf96f29cf9461da40467af79fa09b59da7265eed
Arihant <arihant0996@gmail.com>
2024-02-27 23:33:18 -0400

    Disable eslint warnings

--- frontend/src/components/UserAuth/Register.js
+++ frontend/src/components/UserAuth/Register.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
