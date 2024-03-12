98889e3973294bdc71357a8bc53b5a879daa533e
Abhinav Acharya Tirumala Vinjamuri <ab806657@dal.ca>
2024-03-12 19:53:05 -0300

    added example env file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const Favorite = require('./models/favorite');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors());
